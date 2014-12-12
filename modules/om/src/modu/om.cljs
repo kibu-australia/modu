e8c7304f8a11a9501f6b51adcea94117bfb65107
Thomas Crowley <crowley@kibu.com.au>
2014-12-12 20:46:08 +1100

    Update om.cljs

--- modules/om/src/modu/om.cljs
+++ modules/om/src/modu/om.cljs
@@ -2,24 +2,14 @@
   (:require [com.stuartsierra.component :as component]
             [om.core :as om]))
 
-(defrecord OmRoot [component cursor options]
+(defrecord Om [component cursor options]
   component/Lifecycle
   (start [component]
     (assoc component :om (om/root component cursor options)))
   (stop [component]
     (when-let [target (:target options)]
       (om/detach-root target))
-    (assoc component :om nil)))
+    (dissoc component :om)))
 
-(defn new-om-root [& {:keys [component cursor options] :as opts}]
+(defn new-om [& {:keys [component cursor options] :as opts}]
   (map->OmRoot opts))
-
-(defrecord OmComponent [component data owner]
-  component/Lifecycle
-  (start [component]
-    (assoc component :component (fn [] (component data owner))))
-  (stop [component]
-    (assoc component :component nil)))
-
-(defn new-om-component [& {:keys [component data owner] :as opts}]
-  (map->OmComponent opts))
