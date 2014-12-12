7b0d38cd71d2fde1f5e2c6dd971d5460d4db40af
Thomas Crowley <crowley@kibu.com.au>
2014-12-12 15:05:58 +1100

    Setup project.clj for lein-sub

--- project.clj
+++ project.clj
@@ -1,6 +1,15 @@
-(defproject modu "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject kibu/modu "0.1.0-SNAPSHOT"
+  :description "Clojurescript modules for Component"
+  :url "http://github.com/kibu-australia/modu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+
+  :plugins [[lein-sub "0.2.4"]]
+
+  :sub []
+
+  :aliases {"deploy-all" ["sub" "deploy" "clojars"]
+            "install-all" ["sub" "install"]}
+
+  :profiles {:dependencies [[org.clojure/clojure "1.6.0"]
+                            [org.clojure/tools.namespace "0.2.4"]]})
